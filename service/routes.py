bb27ac37968b4427b52a1c40534a87895a22b8e9
Rami3344 <tsi.rami123@gmail.com>
2026-06-13 12:08:37 -0400

    fix flake

--- service/routes.py
+++ service/routes.py
@@ -4,7 +4,7 @@ Account Service
 This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
-from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
+from flask import jsonify, request, make_response, abort, url_for   # noqa: F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
@@ -98,17 +98,17 @@ def get_account(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-@app.route("/accounts/<int:account_id>",methods=["PUT"])
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     """
     Update an Account
     This endpoint will update an Account based on the posted data
     """
-    app.logger.info("Request to update an Account with id: %s", account_id) 
+    app.logger.info("Request to update an Account with id: %s", account_id)
 
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")  
+        abort( status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found." )  
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
